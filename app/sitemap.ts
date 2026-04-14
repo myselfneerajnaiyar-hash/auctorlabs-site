fcf542faa555cccc1f8b2671d92c2d456c797c33
Neraj Naiyar <myselfneerajnaiyar@gmail.com>
2026-04-14 17:42:47 +0530

    added sitemap for main site

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://auctorlabs.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://auctorlabs.in/auctor-rc",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://rc.auctorlabs.in",
+      lastModified: new Date(),
+    },
+  ];
+}
